5631987406f67e5c85f7937cd415f4c7fd733a2b
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-03-25 15:47:24 +0300

    Added Movement - Camera - Tiles

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+
+    private float horizontalInput;
+    private bool isFacingRight = true;
+
+    [Header("Player Settings")]
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput > 0 && !isFacingRight)
+        {
+            FlipCharacter();
+        }
+        else if (horizontalInput < 0 && isFacingRight)
+        {
+            FlipCharacter();
+        }
+
+        Move();
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    // Rotates the character to the right direction
+    private void FlipCharacter()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
     }
 }

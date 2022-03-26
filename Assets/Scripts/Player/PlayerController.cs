455f7592f501f79360dfd6e76efb58874d0c10df
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-03-26 16:56:23 +0300

    Not Kaldırdım

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -66,9 +66,4 @@ public class PlayerController : MonoBehaviour
     {
         return Physics2D.BoxCast(bxcollider.bounds.center, bxcollider.size, 0.0f, Vector2.down, 0.1f, groundLayer);
     }
-
-    private void AhmedinMg()
-    {
-        Debug.Log("AHMEDIN MGGGG");
-    }
 }

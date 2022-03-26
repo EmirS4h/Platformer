b8747b14bdc6f54011a0b6f953107e561c069bab
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-03-26 19:09:26 +0300

    Added Rotating Saws - Updated Carry Script

--- Assets/Scripts/Platform/Carry.cs
+++ Assets/Scripts/Platform/Carry.cs
@@ -9,6 +9,7 @@ public class Carry : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.transform.SetParent(transform);
+            collision.gameObject.GetComponent<Rigidbody2D>().interpolation = RigidbodyInterpolation2D.None;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -16,6 +17,7 @@ public class Carry : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.transform.SetParent(null);
+            collision.gameObject.GetComponent<Rigidbody2D>().interpolation = RigidbodyInterpolation2D.Interpolate;
         }
     }
 }

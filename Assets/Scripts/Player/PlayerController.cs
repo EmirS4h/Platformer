a04ad9c144766d52a310f83c530620f62fb35ce5
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-03-27 17:52:59 +0300

    Changed GroundChecking - Updated Movement and Jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,12 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator animator;
-    private BoxCollider2D bxcollider;
-    [SerializeField] private LayerMask groundLayer;
 
     [Header("Player Settings")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float coyoteTime;
+    [SerializeField] private float gravityScale;
     [SerializeField] private float acceleration;
     [SerializeField] private float decceleration;
     [SerializeField] private float frictionAmount;
@@ -23,20 +23,33 @@ public class PlayerController : MonoBehaviour
     private float accelRate;
     private float targetSpeed;
     private float horizontalInput;
+    private float lastJumpTime = 0.3f;
+    private float lastGroundedTime;
+
+
+    public bool isGrounded = false;
+    private bool isJumping = false;
     private bool isFacingRight = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        bxcollider = GetComponent<BoxCollider2D>();
     }
+
     private void FixedUpdate()
     {
         Move();
         ApplyGravity();
         ApplyFriction();
+
+        if (isJumping)
+        {
+            Jump();
+            isJumping = false;
+        }
     }
+
     void Update()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -50,13 +63,14 @@ public class PlayerController : MonoBehaviour
         else if (horizontalInput < 0 && isFacingRight)
         {
             FlipCharacter();
-        }    
+        }
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump") && isGrounded && !isJumping)
         {
-            //Jump();
+            isJumping = true;
         }
     }
+
     private void ChangeAnimation()
     {
         // Horizontal input != 0 Running
@@ -66,43 +80,62 @@ public class PlayerController : MonoBehaviour
         // rb.velocity.y < -0.01f ise Falling
         animator.SetBool("Falling", rb.velocity.y < -0.01f);
     }
+
     private void Move()
     {
         targetSpeed = horizontalInput * moveSpeed;
         speedDif = targetSpeed - rb.velocity.x;
         accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
         movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, 0.9f) * Mathf.Sign(speedDif);
-        rb.AddForce(movement* Vector2.right);
+        rb.AddForce(movement * Vector2.right);
     }
-    // Better Jumping
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
     private void ApplyGravity()
     {
-        if (rb.velocity.y < -0.1f)
+        if (rb.velocity.y > 0.01f && !Input.GetButton("Jump"))
         {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            rb.gravityScale = lowJumpMultiplier;
         }
-        else if (rb.velocity.y > 0.1f && !Input.GetButton("Jump"))
+        else if (rb.velocity.y < -0.01f)
         {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            rb.gravityScale = fallMultiplier;
+        }
+        else
+        {
+            rb.gravityScale = gravityScale;
         }
     }
+
     private void ApplyFriction()
     {
-        if (IsGrounded() && Mathf.Abs(horizontalInput) < 0.01f)
+        if (isGrounded && Mathf.Abs(horizontalInput) < 0.01f)
         {
             float amount = Mathf.Min(Mathf.Abs(rb.velocity.x), Mathf.Abs(frictionAmount));
             amount *= Mathf.Sign(rb.velocity.x);
             rb.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
         }
     }
-    // Rotates the character to the right direction
+
     private void FlipCharacter()
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
     }
-    private bool IsGrounded()
-    {
-        return Physics2D.BoxCast(bxcollider.bounds.center, bxcollider.size, 0.0f, Vector2.down, 0.1f, groundLayer);
-    }
+
+    // bu burda dursun belki lazým olur
+
+    //private bool IsGrounded()
+    //{
+    //    bool grounded = Physics2D.BoxCast(bxcollider.bounds.center, bxcollider.size, 0.0f, Vector2.down, 0.1f, groundLayer);
+    //    if (grounded)
+    //    {
+    //        return true;
+    //    }
+    //    return false;
+    //}
 }

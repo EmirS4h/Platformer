296cffd6a99db2a80fafae10aa4408cafcdd1bb1
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-04-12 20:34:30 +0300

    Added UiManager

--- Assets/Scripts/Managers/UiManager.cs
+++ Assets/Scripts/Managers/UiManager.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuManager : MonoBehaviour
+public class UiManager : MonoBehaviour
 {
     [SerializeField] Canvas canvas;
    
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))

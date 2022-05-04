080a36fc7cb93befab076a1ac2e56429843d633f
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-05-04 13:59:24 +0300

    Added Scriptable Object

---
+++ Assets/Scripts/ScriptableObjects/Boost.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Boost", menuName = "Boost", order = 1)]
+public class Boost : ScriptableObject
+{
+    public enum Type
+    {
+        JumpForce,
+        DashForce,
+        MoveSpeed,
+        DoubleJumpBooster,
+        DoubleDashBooster,
+        PermaDashForce,
+        PermaMoveSpeed,
+        PermaJumpForce,
+    }
+    public Type type;
+    public float boostAmount;
+    public float boostTime;
+}

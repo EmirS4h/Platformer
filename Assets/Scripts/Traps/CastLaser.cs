15d4054b8ecc8c00672425973115c687006213c8
Emir <96378991+EmirS4h@users.noreply.github.com>
2022-06-30 19:55:41 +0300

    Added Laser Sfx

--- Assets/Scripts/Traps/CastLaser.cs
+++ Assets/Scripts/Traps/CastLaser.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class CastLaser : MonoBehaviour
 {
     [SerializeField] float defRayDistance = 100.0f;
-    [SerializeField] LineRenderer lnr;
+    [SerializeField] LineRenderer lineRenderer;
     [SerializeField] Transform laserStartPoint;
 
+    [SerializeField] GameObject startVfx;
+    [SerializeField] GameObject endVfx;
+
+    [SerializeField] List<ParticleSystem> particles = new List<ParticleSystem>();
+
+    private void Start()
+    {
+        FillLists();
+        for (int i = 0; i < 2; i++)
+        {
+            particles[i].Play();
+        }
+    }
     private void Update()
     {
         ShootLaser();
@@ -15,16 +28,42 @@ public class CastLaser : MonoBehaviour
 
     private void ShootLaser()
     {
-        lnr.SetPosition(0, laserStartPoint.position);
+        lineRenderer.SetPosition(0, laserStartPoint.position);
+        startVfx.transform.position = laserStartPoint.position;
 
-        RaycastHit2D hit = Physics2D.Raycast(laserStartPoint.transform.position, laserStartPoint.transform.right);
+        RaycastHit2D hit = Physics2D.Raycast(laserStartPoint.position, laserStartPoint.right);
         if (hit)
         {
-            lnr.SetPosition(1, hit.point);
+            lineRenderer.SetPosition(1, hit.point);
+            if (hit.collider.CompareTag("Player"))
+            {
+                PlayerController.Instance.playerLife.Die();
+            }
         }
         else
         {
-            lnr.SetPosition(1, laserStartPoint.right * defRayDistance);
+            lineRenderer.SetPosition(1, laserStartPoint.right * defRayDistance);
+        }
+        endVfx.transform.position = lineRenderer.GetPosition(1);
+    }
+    private void FillLists()
+    {
+        for (int i = 0; i< startVfx.transform.childCount; i++)
+        {
+            var ps = startVfx.transform.GetChild(i).GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                particles.Add(ps);
+            }
+        }
+        for (int i = 0; i< endVfx.transform.childCount; i++)
+        {
+            var ps = endVfx.transform.GetChild(i).GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                particles.Add(ps);
+            }
         }
     }
 }
+
